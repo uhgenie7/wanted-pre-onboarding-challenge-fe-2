6843f4c99972eff398efd6819a50aa45e9e0ef13
HyejinUm <uhj1993@naver.com>
2022-08-23 11:28:20 +0900

    feat: (#2) test JsDoc input

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * "할일제목: ${title}, 할일내용: ${content}"을 반환하는 함수
+ *
+ * @param {string} title - 할일 제목
+ * @param {string} content - 할일 내용
+ * @returns {string} - 할일제목: title, 할일내용: content
+ */
+function createTodoItem(title, content) {
+  return `할일제목: ${title}, 할일내용: ${content}`;
+}
