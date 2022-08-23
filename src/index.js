ff0739cfc0bf11b27e991f5d7df64545edc93ed9
HyejinUm <uhj1993@naver.com>
2022-08-23 12:29:35 +0900

    feat: (#3) todos modeling ... ?

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
+const model = {
+  todos: [
+    {
+      id: "1",
+      content: "데이터 모델링부터 막힙니다. 이렇게 하는 게 맞나? 에궁...",
+      done: false,
+      category: "model",
+      hashtag: "#model",
+    },
+    {
+      id: "2",
+      content: "jsdoc을 설치해봤습니다. 이게 맞나? 의 연속",
+      done: true,
+      category: "jsdoc",
+    },
+  ],
+};
+
 /**
- * "할일제목: ${title}, 할일내용: ${content}"을 반환하는 함수
+ * "할일내용: ${content}"을 반환하는 함수
  *
- * @param {string} title - 할일 제목
+ * @param {category} category - 카테고리
  * @param {string} content - 할일 내용
+ * @param {?hashtag} hashtag - 해시 태그
  * @returns {string} - 할일제목: title, 할일내용: content
  */
-function createTodoItem(title, content) {
-  return `할일제목: ${title}, 할일내용: ${content}`;
+function createTodoItem(content, category, hashtag) {
+  return `${category} : 할일내용: ${content} 태그: ${hashtag}`;
 }
