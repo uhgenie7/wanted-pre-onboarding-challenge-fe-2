6257ebb8b7de9c228151c9c5a67f1d505ea62ed8
HyejinUm <uhj1993@naver.com>
2022-08-24 18:37:27 +0900

    feat: (#3) type definition 추가

--- src/todolist.js
+++ src/todolist.js
@@ -1,54 +1,56 @@
 /**
- * @namespace
+ * Todo type definition
+ * @typedef {Object} Todo
+ * @property {string} id - id
+ * @property {string} content - 할일 내용
+ * @property {boolean} isDone - 완료 여부
+ * @property {string} category - 카테고리
+ * @property {string[]} [hashtag] - 해시태그
  */
-const TodoList = {
-  /**
-   * Todo Modeling
-   * @type {Object}
-   * @example Todo {
-   *    id,
-   *    content,
-   *    isDone,
-   *    category,
-   *    hashtag
-   * }
-   */
-  Todo: { id, content, isDone, category, hashtag },
-  /**
-   * 모든 할일을 조회하는 함수.
-   * @function
-   */
-  getTodoList: function () {},
 
-  /**
-   * ID를 기반으로 특정 할 일을 조회할 수 있는 함수.
-   * @function
-   * @param {string} id - todo id
-   */
-  getTodoItemById: function (id) {},
+/**
+ * 모든 할일을 조회하는 함수.
+ * @function
+ * @returns {Todo[]}
+ */
+const getTodoList = () => {};
 
-  /**
-   * 할일을 생성하는 함수
-   * @function
-   * @param {string} category - 카테고리
-   * @param {string} content - 할일 내용
-   * @param {string} [hashtag] - 해시 태그
-   */
-  createTodoItem: function (category, content, hashtag) {},
+/**
+ * ID를 기반으로 특정 할 일을 조회할 수 있는 함수.
+ * @function
+ * @param {string} id - todo id
+ * @returns {Todo}
+ */
+const getTodoItemById = (id) => {};
 
-  /**
-   * 할일을 수정하는 함수
-   * @function
-   * @param {string} category - 카테고리
-   * @param {string} content - 할일 내용
-   * @param {string} [hashtag] - 해시 태그
-   */
-  updateTodoItem: function (category, content, hashtag) {},
+/**
+ * 할일을 생성하는 함수
+ * @function
+ * @param {string} category - 카테고리
+ * @param {string} content - 할일 내용
+ * @param {string[]} [hashtag] - 해시 태그
+ */
+const createTodoItem = (category, content, hashtag) => {};
 
-  /**
-   * 할일을 삭제하는 함수
-   * @function
-   * @param {string} id - todo id
-   */
-  deleteTodoItem: function (id) {},
-};
+/**
+ * 할일을 수정하는 함수
+ * @function
+ * @param {string} id - todo id
+ * @param {string} category - 카테고리
+ * @param {string} content - 할일 내용
+ * @param {string[]} [hashtag] - 해시 태그
+ */
+const updateTodoItem = (id, category, content, hashtag) => {};
+
+/**
+ * 할일을 삭제하는 함수
+ * @function
+ * @param {string} id - todo id
+ */
+const deleteTodoItem = (id) => {};
+
+/**
+ * 할일을 모두 삭제하는 함수
+ * @function
+ */
+const clearTodoList = () => {};
